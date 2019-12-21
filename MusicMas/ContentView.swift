538905f06b49628acc675a80df7126995e22be14
Juan Andrés Salinas Toro <jandres.saltor@gmail.com>
2019-12-21 17:47:25 -0300

    commit inicial

---
+++ MusicMas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicMas
+//
+//  Created by thomas anderson on 12/21/19.
+//  Copyright © 2019 juan Andrés. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
