235c6507b075a266011dd002d670e94b4e31ac24
Juan Andrés Salinas Toro <jandres.saltor@gmail.com>
2019-12-21 20:10:16 -0300

    creo simple template

--- MusicMas/ContentView.swift
+++ MusicMas/ContentView.swift
@@ -8,9 +8,27 @@
 
 import SwiftUI
 
+
+struct CircleImage: View {
+    var body: some View {
+        Image("audifonos").resizable().scaledToFit()
+            .frame(width: 100.0, height: 100.0).clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+       
+        VStack(alignment: .leading) {
+                CircleImage()
+                               .offset(y: -130)
+                               .padding(.bottom, -130)
+                Text("Music Mas").font(.title).italic().foregroundColor(.accentColor)
+            }.offset(y: 0)
+            .padding(.bottom, 30)
+        
     }
 }
 
